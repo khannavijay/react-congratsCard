f28461e997a95b03b94ece298cb96e8a9e19c49f
khannavijay <tkhannavijay@gmail.com>
2021-05-22 14:23:12 +0000

    final commit

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const element = (
   // Write your code here.
-  return(
+
   <div className="bg-container">
-  <h1>Congratulations</h1>
+    <h1 className="heading">Congratulations</h1>
     <div className="card-container">
-        <img className="user" src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" alt=""/>
-        <h1>Kiran V</h1>
-        <p>Vishnu institute of Computer Education and Technology, Bhimavaram.</p>
-        <img className="watch" src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" alt=""/>
+      <img
+        className="user"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        alt=""
+      />
+      <h1 className="name">Kiran V</h1>
+      <p>Vishnu institute of Computer Education and Technology, Bhimavaram.</p>
+      <img
+        className="watch"
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        alt=""
+      />
     </div>
-  </div>)
+  </div>
 );
 
 ReactDOM.render(element, document.getElementById("root"));

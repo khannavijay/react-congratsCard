02548b8084f11e8e355d7de900ee3c8c2eb5fea9
khannavijay <tkhannavijay@gmail.com>
2021-05-22 13:50:56 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  // Write your code here.
+  return(
+  <div className="bg-container">
+  <h1>Congratulations</h1>
+    <div className="card-container">
+        <img className="user" src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" alt=""/>
+        <h1>Kiran V</h1>
+        <p>Vishnu institute of Computer Education and Technology, Bhimavaram.</p>
+        <img className="watch" src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" alt=""/>
+    </div>
+  </div>)
+);
+
+ReactDOM.render(element, document.getElementById("root"));
